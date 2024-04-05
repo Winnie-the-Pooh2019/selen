8227c321554b8bc41100a4f4f751a61fc37f0bb2
Winnie-the-Pooh2019 <ivan.duvanov.3@gmail.com>
2024-04-05 19:08:31 +0700

    removed redundant implementations

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,9 @@ repositories {
 }
 
 dependencies {
-//    testImplementation("org.jetbrains.kotlin:kotlin-test")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.seleniumhq.selenium:selenium-java:4.19.1")
-//    implementation("io.github.bonigarcia:webdrivermanager:5.7.0")
-
-
-//    implementation("org.slf4j:slf4j-api:2.0.9")
-//    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
 }
 
 
